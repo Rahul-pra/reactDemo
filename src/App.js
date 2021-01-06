7cd25e5993d005083845449d75bebcedc4a7e217
rahul-prajapati2020 <rahul.prajapati@acquaintsoft.com>
2021-01-06 14:54:50 +0530

    Added CSS, images , font

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +16,13 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        header
       </header>
+      
+      <footer>
+        footer
+      </footer>
     </div>
   );
 }
